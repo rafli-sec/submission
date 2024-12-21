8a765b88ba9a0f017488ad73fee47498d7c1eb00
rafli-sec <raflidiga1109@gmail.com>
2024-12-21 12:44:44 +0800

    Update InferenceService.js
    menganti response

--- src/services/InferenceService.js
+++ src/services/InferenceService.js
@@ -1,39 +1,26 @@
-const tf = require("@tensorflow/tfjs-node");
-
-const InputError = require("../exceptions/InputError");
+const tf = require('@tensorflow/tfjs-node');
+const InputError = require('../exceptions/InputError');
 
 async function predictClassification(model, image) {
   try {
     const tensor = tf.node
-      .decodeImage(image)
+      .decodeJpeg(image)
       .resizeNearestNeighbor([224, 224])
       .expandDims()
       .toFloat();
 
     const prediction = model.predict(tensor);
-    const score = await prediction.data();
-    const confidenceScore = Math.max(...score) * 100;
 
-    console.log("score: ", score);
-    console.log("confidenceScore: ", confidenceScore);
-
-    // Model akan mengembalikan array dengan rentang nilai 0 hingga 1. Di mana jika rentang nilainya di atas 50% diklasifikasikan sebagai Cancer, jika di bawah atau sama dengan 50% diklasifikasikan sebagai Non-cancer.
-    const label = confidenceScore > 50 ? "Cancer" : "Non-cancer";
-
-    let suggestion;
-
-    if (label === "Cancer") {
-      suggestion =
-        "Bersabar, jangan panik. Segera periksakan diri ke dokter untuk mendapatkan penanganan lebih lanjut.";
-    } else {
-      suggestion =
-        "Tetap jaga kesehatan dan pola hidup sehat. Jangan lupa untuk rutin periksa kesehatan.";
-    }
+    const label = prediction.dataSync()[0] > 0.5 ? 'Cancer' : 'Non-cancer';
+    console.log(label);
+    const suggestion =
+      label === 'Cancer'
+        ? 'Segera periksa ke dokter!'
+        : 'Penyakit kanker tidak terdeteksi.';
 
     return {
-      confidenceScore,
       label,
-      suggestion: suggestion,
+      suggestion,
     };
   } catch (error) {
     throw new InputError(`Terjadi kesalahan dalam melakukan prediksi`);
